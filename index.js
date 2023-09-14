c8be3972482e7772640052067537037931262858
franco <rojasfranco204@gmail.com>
2023-09-14 00:46:51 -0300

    proyectofinal

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const helmet = require('helmet');
 const {DBTest} = require('./database.js');
 
 const Posteo = require ('./posteoModel.js')
@@ -7,7 +8,7 @@ const Posteo = require ('./posteoModel.js')
 const app = express()
 const PUERTO = process.env.PUERTO
 app.set('view engine','ejs');
-
+app.use(helmet())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
---
+++ proyecto_final.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 14-09-2023 a las 03:45:28
+-- Versión del servidor: 8.0.30
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_final`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `posteos`
+--
+
+CREATE TABLE `posteos` (
+  `id` int NOT NULL,
+  `titulo` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `descripcion` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `link` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `fecha` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `creacion` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `posteos`
+--
+ALTER TABLE `posteos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `posteos`
+--
+ALTER TABLE `posteos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
